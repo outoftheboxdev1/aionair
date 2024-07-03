04a14369cd329dad82f101a5bbf80ca3ba45b0d6
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2024-07-02 22:30:05 -0400

    Brand

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -23,7 +23,7 @@ const LeftSidebar = () => {
       <nav className="flex flex-col gap-6">
         <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
           <Image src="/icons/miclogo.svg" alt="logo" width={23} height={27} />
-          <h1 className="text-24 font-extrabold text-white max-lg:hidden">OnAirAI</h1>
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">YouPoderAI</h1>
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
