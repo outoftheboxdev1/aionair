8c00e31f98348cb94a47caef7e8182350768fb14
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2024-07-06 14:59:04 -0400

    Update users.ts

--- convex/users.ts
+++ convex/users.ts
@@ -110,7 +110,7 @@ export const getTotalPodcastsOfUser = query({
   handler: async (ctx, args) => {
     const user = await ctx.db
       .query("users")
-      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
       .unique();
 
     if (!user) {
