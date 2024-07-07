9e1ebdb2b493fd4e6d1954b136d901b290fc6523
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2024-07-07 17:18:10 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   },
   keywords: ["podcast", "generate podcast", "create podcast with ai", "multilingual podcasts"],
   openGraph: {
-    images: '/images/podcast-ai.png',
+    images: '/public/images/podcast-ai.PNG',
   },
 };
 
